36f3bd4df14e86707d6826d0c2ce7f3b310ace65
Oriane <orianegalmar@gmail.com>
2019-11-25 16:39:19 +0100

    Auto stash before merge of "master" and "origin/master"

--- formation-data-jpa-spring-advanced/src/main/java/sopra/formation/repository/IFiliereRepository.java
+++ formation-data-jpa-spring-advanced/src/main/java/sopra/formation/repository/IFiliereRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sopra.formation.model.Filiere;
 
 public interface IFiliereRepository extends JpaRepository<Filiere, Long> {
+
 }
